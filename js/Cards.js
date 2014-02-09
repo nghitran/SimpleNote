10959867f95297bc61077f56be90b57c4fdba5f2
Nghi Tran <tpmnghi@gmail.com>
2014-02-09 15:44:40 +0700

    update project's information

--- js/Cards.js
+++ js/Cards.js
@@ -1,145 +1,97 @@
+/**
+ * Created by supertux on 1/24/14.
+ */
 
 var Cards = function(_options) {
     var _this = this,
-        elContainer = null, cardElements = [], drawerWidth = 0, cardWidth = 0, currentIndex = -1,
-        hasDrawer = false, transitionDuration = 0, transitionEasing = "",
-        onMove = null;
+        onMove = null,
+        _currentView = null,
+        stack = [];
 
-    var CLASS_WHEN_ACTIVE = "active",
-        CLASS_DRAWER = "drawer",
-        DEFAULT_TRANSITION_DURATION = 400,
-        DEFAULT_TRANSITION_EASING = "ease";
-        
-    this.CARDS = {};
-    
-    this.init = function(options) {
-        !options && (options = {});
-        
-        onMove = options.onMove;
-        elContainer = options.container || document.querySelector(".cards");
-        
-        elContainer.style.cssText += '; position: relative; overflow: hidden; min-height: 100%;';
-        
-        cardWidth = drawerWidth = elContainer.offsetWidth;
-        cardElements = elContainer.getElementsByClassName("card");
-        
-        transitionDuration = options.transitionDuration || DEFAULT_TRANSITION_DURATION;
-        transitionEasing = options.transitionEasing || DEFAULT_TRANSITION_EASING;
-        
-        initCards();
+    var transitions = {
+        'right-left': 'card-right',
+        'popup': 'card-bottom'
     };
 
-    this.goTo = function(index) {
-        if (typeof index == "string") {
-            index = getIndexById(index);
+    var CLASS_WHEN_ACTIVE = "active";
+
+    this.CARDS = {};
+
+    this.init = function(options) {
+        !options && (options = {});
+
+        onMove = options.onMove;
+        initCards();
+    }
+
+    this.goTo = function(index, transitionEffect) {
+        transitionEffect = typeof transitionEffect !== 'undefined' ? transitionEffect : 'right-left';
+
+        stack = stack.filter(function(item) {
+            return item.view != index;
+        });
+
+        var next = document.getElementById(index);
+        next.classList.remove(transitions[transitionEffect] || transitionEffect);
+        var zIndexValue = stack.length > 0 ? stack[stack.length-1].zIndex + 1 : 1;
+        stack.push({ view: index, transition: transitionEffect, zIndex: zIndexValue});
+        next.style.zIndex = zIndexValue;
+        _currentView = index;
+    }
+
+    this.back = function back() {
+        if (stack.length < 2) {
+            return;
         }
-        
-        if (index < 0 || index >= cardElements.length) {
-            return _this;
+
+        var currentView = stack.pop();
+        var current = document.getElementById(currentView.view);
+        var nextView = stack[stack.length - 1];
+        var transition = currentView.transition;
+        current.classList.add(transitions[transition] || transition);
+        _currentView = nextView.view;
+    };
+    
+    this.home = function home() {
+        while (stack.length > 1) {
+            this.back();
         }
-        
-        for (var i=0, l=cardElements.length; i<l; i++) {
-            var pos = 0,
-                zIndex = 10,
-                el = cardElements[i];
+    }
 
-            if (i < index) {
-                pos = -((index-i)*cardWidth);
-            } else if (i > index) {
-                pos = (i-index)*cardWidth;
-            } else {
-                pos = 0;
-                zIndex = 100;
-            }
-
-            if (hasDrawer) {
-                if (index == 0 && i == 1) {
-                    pos = pos - cardWidth + drawerWidth;
-                }
-            }
-
-            el.style.cssText += "; z-index: " + zIndex + ";" +
-                                 "-moz-transform: translate3d(" + pos + "px, 0, 0); " +
-                                 "-webkit-transform: translate3d(" + pos + "px, 0, 0); ";
-        }
-        
-        cardElements[currentIndex].classList.remove(CLASS_WHEN_ACTIVE);
-        document.body.classList.remove("card-" + cardElements[currentIndex].id);
-        currentIndex = index;
-        cardElements[currentIndex].classList.add(CLASS_WHEN_ACTIVE);
-        document.body.classList.add("card-" + cardElements[currentIndex].id);
-        
-        onMove && onMove(currentIndex);
-
-        return _this;
+    this.currentView = function currentView() {
+        return _currentView != null ? _currentView : '';
     };
 
     function initCards() {
-        var defaultIndex = 0;
+        var defaultIndex = "",
+            cardElements = document.getElementsByClassName("card");
 
         for (var i=0, l=cardElements.length; i<l; i++) {
             var el = cardElements[i];
 
-            el.style.cssText += "; position: absolute; top: 0; left: 0; width: " + cardWidth + "px; min-height: 100%;";
-
-            if (el.className.indexOf(CLASS_WHEN_ACTIVE) !== -1) {
-                currentIndex = i;
-                defaultIndex = i;
-            }
-            if (el.className.indexOf(CLASS_DRAWER) !== -1) {
-                hasDrawer = true;
-                el.isDrawer = true;
-                drawerWidth -= el.getAttribute("data-gutter");
-                el.style.cssText += "; width: " + drawerWidth + "px;";
-            }
-
             _this.CARDS[el.id.toUpperCase().replace(/-/g, "_")] = el.id;
 
-            addDefaultButtons(el, i);
+            addDefaultButtons(el);
+
+            if (el.className.indexOf(CLASS_WHEN_ACTIVE) !== -1) {
+                _currentView = el.id;
+                defaultIndex = el.id;
+            }
         }
 
         _this.goTo(defaultIndex);
-
-        window.setTimeout(enableAnimation, 0);
     }
 
-    function addDefaultButtons(el, index) {
+    function addDefaultButtons(el) {
         var buttons = el.getElementsByClassName("card-prev");
-        for (i=0; i<buttons.length; i++) {
-            buttons[i].addEventListener("click", function(){
-                _this.goTo(index-1);
-            });
-        }
-        buttons = el.getElementsByClassName("card-next");
-        for (i=0; i<buttons.length; i++) {
-            buttons[i].addEventListener("click", function(){
-                _this.goTo(index+1);
-            });
-        }
-    }
-    
-    function enableAnimation() {
-        for (var i=0, l=cardElements.length; i<l; i++) {
-            var duration = transitionDuration;
-            
-            if (cardElements[i].isDrawer) {
-                // duration should be proportionate so that there won't be the movement gap
-            }
-            
-            cardElements[i].style.cssText += "; -moz-transition: all " + duration + "ms " + transitionEasing + ";" +
-                                             "; -webkit-transition: all " + duration + "ms " + transitionEasing + ";";
-        }
-    }
-    
-    function getIndexById(cardId) {
-        for (var i=0, l=cardElements.length; i<l; i++) {
-            if (cardElements[i].id == cardId) {
-                return i;
+        for (var i=0; i<buttons.length; i++) {
+            if (buttons[i].classList.contains("default-button")) {
+                buttons[i].addEventListener("click", function(){
+                    _this.back();
+                });
             }
         }
-
-        return -1;
     }
 
     _this.init(_options);
-};
+}
